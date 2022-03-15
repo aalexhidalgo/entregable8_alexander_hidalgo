0f0093b370b29b4c3f38b0a974589713a37465d6
aalexhidalgo <91455208+aalexhidalgo@users.noreply.github.com>
2022-03-15 21:16:20 +0100

    15/03/2022 Falta arte

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,25 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class AudioManager : MonoBehaviour
 {
     //Lista de canciones
     public AudioClip[] MusicArray;
     public int CurrentSong;
+    public string[] NameSong;
 
     private AudioSource PlayerAudioSource;
+    public TextMeshProUGUI TitleSong;
 
     // Start is called before the first frame update
     void Start()
     {
         //Accedemos a la component AudioSource
         PlayerAudioSource = GetComponent<AudioSource>();
+        //Iniciamos con la primera canción en la lista
+        PlayerAudioSource.PlayOneShot(MusicArray[CurrentSong]);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Actualizamos el nombre de la canción por la que se está escuchando en ese momento
+        TitleSong.text = NameSong[CurrentSong];
+    }
+
+    public void Next()
+    {
+        CurrentSong++;
+
+        if(CurrentSong >= MusicArray.Length)
+        {
+            CurrentSong = 0;
+        }
+
+        PlayerAudioSource.Stop();
+        PlayerAudioSource.PlayOneShot(MusicArray[CurrentSong]);
+    }
+
+    public void Back()
+    {
+        CurrentSong--;
+
+        if (CurrentSong < 0)
+        {
+            CurrentSong = MusicArray.Length - 1;
+        }
+
+        PlayerAudioSource.Stop();
+        PlayerAudioSource.PlayOneShot(MusicArray[CurrentSong]);
+    }
+
+    public void Pause()
+    {
+        PlayerAudioSource.Pause();
+    }
+
+    public void Play()
+    {
+        PlayerAudioSource.UnPause();
+    }
+
+    public void ChooseRandomly()
+    {
+        PlayerAudioSource.Stop();
+        CurrentSong = Random.Range(0, MusicArray.Length);
+        PlayerAudioSource.PlayOneShot(MusicArray[CurrentSong]);
     }
 }
+
